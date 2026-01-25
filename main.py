8dbb18826311c2ce6a57323696d2ce436ada4a86
modybon14 <mohamedfahmy3020@gmail.com>
2026-01-25 14:49:25 -0500

    updated requirments and routes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env") # loads the .env variables into the os envirnoment
+from routes import base
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {
-        "message" : "Hello World"
-    }
+app.include_router(base.base_router)
